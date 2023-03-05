8c8e644cd9002ea9866a6b62acba080646469c0b
Nita1001 <devinedevops@gmail.com>
2023-03-05 11:29:03 +0200

    fixed logOut button disappearance after refreshing

--- src/contexts/LogInContext.js
+++ src/contexts/LogInContext.js
@@ -1,4 +1,4 @@
-import { useState, createContext } from 'react';
+import { useState, createContext, useEffect } from 'react';
 import axios from 'axios';
 
 const LogInContext = createContext({
@@ -8,25 +8,35 @@ const LogInContext = createContext({
 });
 
 const LogInProvider = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(
+        JSON.parse(localStorage.getItem('isLoggedIn')) || false
+    );
 
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    useEffect(() => {
+        localStorage.setItem('isLoggedIn', JSON.stringify(isLoggedIn));
+    }, [isLoggedIn]);
 
     const logIn = () => setIsLoggedIn(true);
 
     const logOut = async () => {
-
         try {
             const userToken = localStorage.getItem('userToken');
-            const response = await axios.get(`https://64022104ab6b7399d0b48fee.mockapi.io/users/${userToken}`);
+            const response = await axios.get(
+                `https://64022104ab6b7399d0b48fee.mockapi.io/users/${userToken}`
+            );
             const user = response.data;
             user.isLoggedIn = false;
-            await axios.put(`https://64022104ab6b7399d0b48fee.mockapi.io/users/${user.id}`, user);
+            await axios.put(
+                `https://64022104ab6b7399d0b48fee.mockapi.io/users/${user.id}`,
+                user
+            );
             setIsLoggedIn(false);
             localStorage.removeItem('userToken');
         } catch (error) {
             console.error(error);
         }
     };
+
     return (
         <LogInContext.Provider value={{ isLoggedIn, logIn, logOut }}>
             {children}
