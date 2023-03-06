b1b32575c1506d2c8a1dc0f4b8e143b5f65c9c4d
Nita1001 <devinedevops@gmail.com>
2023-03-06 15:11:09 +0200

    API

--- src/api/userServices.js
+++ src/api/userServices.js
@@ -1,26 +1,6 @@
 import axios from 'axios';
 import { USERS_URL, API_ERROR_MESSAGE } from '../utils/constants'
 
-export const updateUser = async (user) => {
-  try {
-    const response = await axios.put(`${USERS_URL}/${user.id}`, user);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw new Error(API_ERROR_MESSAGE);
-  }
-};
-
-export const deleteUser = async (userId) => {
-  try {
-    const response = await axios.delete(`${USERS_URL}/${userId}`);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw new Error(API_ERROR_MESSAGE);
-  }
-};
-
 export const createUser = async (user) => {
   try {
     const response = await axios.post(`${USERS_URL}`, user);
@@ -30,6 +10,30 @@ export const createUser = async (user) => {
     throw new Error(API_ERROR_MESSAGE);
   }
 };
+
+export const createSchedule = async (id, updatedData) => {
+  try {
+    const currentUser = await getUserById(id);
+    console.log('API current user', currentUser);
+
+    return currentUser.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error(API_ERROR_MESSAGE);
+  }
+};
+
+export const logInUser = async (email, password) => {
+  try {
+    const response = await axios.get(USERS_URL);
+    const user = response.data.find((user) => user.email === email && user.password === password);
+    return user;
+  } catch (error) {
+    console.error(error);
+    throw new Error(API_ERROR_MESSAGE);
+  }
+};
+
 
 export const getUsers = async () => {
   try {
@@ -51,11 +55,20 @@ export const getUserById = async (userId) => {
   }
 };
 
-export const logInUser = async (email, password) => {
+export const getUsersSchedule = async (id) => {
   try {
-    const response = await axios.get(USERS_URL);
-    const user = response.data.find((user) => user.email === email && user.password === password);
-    return user;
+    const user = await getUserById(id);
+    return user.schedules;
+  } catch (error) {
+    console.error(error);
+    throw new Error(API_ERROR_MESSAGE);
+  }
+};
+
+export const updateUser = async (user) => {
+  try {
+    const response = await axios.put(`${USERS_URL}/${user.id}`, user);
+    return response.data;
   } catch (error) {
     console.error(error);
     throw new Error(API_ERROR_MESSAGE);
@@ -65,7 +78,16 @@ export const logInUser = async (email, password) => {
 export const updateUsersData = async (id, updatedData) => {
   try {
     const response = await axios.put(`${USERS_URL}/${id}`, updatedData);
-    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error(API_ERROR_MESSAGE);
+  }
+};
+
+export const deleteUser = async (userId) => {
+  try {
+    const response = await axios.delete(`${USERS_URL}/${userId}`);
     return response.data;
   } catch (error) {
     console.error(error);
