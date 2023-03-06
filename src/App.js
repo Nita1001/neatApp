5c419bf386a3d6bc45c3229dd9c2a8bfa9261876
Nita1001 <devinedevops@gmail.com>
2023-03-06 15:10:48 +0200

    providers separation

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home, About, SignUp, LogIn, Profile, Schedule, NotFound } from './pages';
 import SharedLayout from './components/layout/SharedLayout';
-import { LogInProvider } from './contexts/LogInContext';
-import { SelectedDateProvider } from './contexts/SelectedDateContext';
+
 function App() {
   return (
-    <LogInProvider>
-      <SelectedDateProvider>
-
-        <BrowserRouter>
-          <div className="text-center">
-            <Routes>
-              <Route path="/" element={<SharedLayout />}>
-                <Route index element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/signUp" element={<SignUp />} />
-                <Route path="/logIn" element={<LogIn />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/schedule" element={<Schedule />} />
-                <Route path="*" element={<NotFound />} />
-              </Route>
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </SelectedDateProvider>
-    </LogInProvider>
+    <BrowserRouter>
+      <div className="text-center">
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/logIn" element={<LogIn />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/schedule" element={<Schedule />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { LogInProvider } from './contexts/LogInContext';
+import { SelectedDateProvider } from './contexts/SelectedDateContext';
+import TasksContextProvider from './contexts/TasksContext';
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LogInProvider>
+      <SelectedDateProvider>
+        <TasksContextProvider>
+          <App />
+        </TasksContextProvider>
+      </SelectedDateProvider>
+    </LogInProvider>
   </React.StrictMode>
 );
