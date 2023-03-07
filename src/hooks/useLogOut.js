54d7ed13f8d22da4d9a99556641de4f019b8d79b
Nita1001 <devinedevops@gmail.com>
2023-03-07 22:36:26 +0200

    minor

--- src/hooks/useLogOut.js
+++ src/hooks/useLogOut.js
@@ -2,15 +2,17 @@ import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTaskGlobalContext } from '../contexts/TasksContext';
 import { LogInContext } from '../contexts/LogInContext';
-
+import useMeetingHoursList from './useMeetingHoursList';
 const useLogOut = () => {
     const { logOut } = useContext(LogInContext);
     const { setShowScheduled } = useTaskGlobalContext();
     const navigate = useNavigate();
+    const { setSelectedTime } = useMeetingHoursList();
 
     const handleLogout = () => {
-        logOut();
         setShowScheduled(false);
+        setSelectedTime(null);
+        logOut();
         navigate('/logIn');
     };
 
