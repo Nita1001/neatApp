2c7b44afe11fbbf6964a54f9ae34a1d25471ce85
Nita1001 <devinedevops@gmail.com>
2023-03-09 17:08:16 +0200

    scheduleBackground

--- src/components/layout/SharedLayout.js
+++ src/components/layout/SharedLayout.js
@@ -1,20 +1,33 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import MainNav from './MainNav'
-import MobileMainNav from './MobileMainNav'
-import useViewport from '../../hooks/useViewport'
+import React, { useState, useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import MainNav from './MainNav';
+import MobileMainNav from './MobileMainNav';
+import useViewport from '../../hooks/useViewport';
+import backgrounds from '../../utils/backgrounds';
+
 const SharedLayout = () => {
     const { width } = useViewport();
     const breakpoint = 700;
+    const location = useLocation();
+    const [pageClass, setPageClass] = useState('');
+
+    useEffect(() => {
+        const getPageClass = async () => {
+            const path = location.pathname;
+            const cssClass = backgrounds[path] || '';
+            setPageClass(cssClass);
+        };
+        getPageClass();
+    }, [location]);
 
     return (
         <>
             {width < breakpoint ? <MobileMainNav /> : <MainNav />}
-            <div className='container'>
+            <div className={`container ${pageClass}`}>
                 <Outlet />
             </div>
         </>
-    )
-}
+    );
+};
 
-export default SharedLayout
+export default SharedLayout;
---
+++ src/utils/backgrounds.js
@@ -0,0 +1,8 @@
+
+const backgrounds = {
+    '/': '',
+    '/schedule': 'scheduleBackground',
+    // Add more entries for each page here
+};
+
+export default backgrounds;
