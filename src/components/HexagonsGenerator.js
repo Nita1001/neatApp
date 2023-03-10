b06b8e316a9699c4c4663f46c588c3310d12c96f
Nita1001 <devinedevops@gmail.com>
2023-03-10 21:54:07 +0200

    experimenting positions

--- src/components/HexagonsGenerator.js
+++ src/components/HexagonsGenerator.js
@@ -1,22 +1,52 @@
 import React from 'react'
 import Hexagon from "./Hexagon";
 import crown from '../assets/icons/crown-colored.png'
+import rocket from '../assets/icons/rocket-front-gradient.png'
+import fire from '../assets/icons/fire-front-gradient.png'
+
+import background from '../assets/images/hexagonBackground.jfif'
 
 const images = [
   crown,
-  'https://images.unsplash.com/photo-1637342672517-47dbb7f83f94?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzODc3MjM5NA&ixlib=rb-1.2.1&q=80&w=400',
+  rocket,
+  fire,
 ];
-const titles = ['sky', 'King slayer']
+const titles = ['King slayer', 'sky']
+
+
 const HexagonsGenerator = () => {
+
+  const badges = ['crown', 'rocket', 'fire']
+
   const isIcon = true;
   return (
+
     <div className='hexagonsContainer'>
-      <Hexagon x={0} y={0} imageSrc={images[1]} />
-      <Hexagon x={0} y={0} imageSrc={images[0]} title={titles[1]} isIcon={isIcon} />
-      <Hexagon x={-1} y={1} imageSrc={images[1]} title={titles[0]} />
-      <Hexagon x={1} y={1} imageSrc={images[1]} title={titles[0]} />
-      <Hexagon x={-2} y={2} imageSrc={images[1]} title={titles[0]} />
-      <Hexagon x={0} y={2} imageSrc={images[1]} title={titles[0]} />
+      {
+        badges.map((badge, index) => {
+          console.log('badge', badge, index);
+          return (
+            <>
+              <Hexagon x={index % 2 === 0 ? index : -index} y={index} imageSrc={background} title={titles[index]} />
+              <Hexagon x={index % 2 === 0 ? index : -index} y={index} imageSrc={images[index]} isIcon={isIcon} />
+            </>
+          )
+        })
+      }
+
+
+      {/* <Hexagon x={0} y={0} imageSrc={images[0]} title={titles[1]} />
+      <Hexagon x={0} y={0} imageSrc={images[1]} isIcon={isIcon} /> */}
+      {/* <Hexagon x={-1} y={1} imageSrc={images[0]} title={titles[0]} />
+      <Hexagon x={-1} y={1} imageSrc={images[2]} isIcon={isIcon} /> */}
+
+
+
+      {/* 
+      <Hexagon x={1} y={1} imageSrc={images[0]} title={titles[0]} />
+
+      <Hexagon x={-2} y={2} imageSrc={images[0]} title={titles[0]} />
+      <Hexagon x={0} y={2} imageSrc={images[0]} title={titles[0]} /> */}
 
     </div>
   );
