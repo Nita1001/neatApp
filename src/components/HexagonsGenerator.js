c8b7fcda543e942880c6380510ea91bdf04f7a82
Nita1001 <devinedevops@gmail.com>
2023-03-10 22:10:31 +0200

    experimenting positions

--- src/components/HexagonsGenerator.js
+++ src/components/HexagonsGenerator.js
@@ -3,32 +3,33 @@ import Hexagon from "./Hexagon";
 import crown from '../assets/icons/crown-colored.png'
 import rocket from '../assets/icons/rocket-front-gradient.png'
 import fire from '../assets/icons/fire-front-gradient.png'
-
+import star from '../assets/icons/star-front-gradient.png'
 import background from '../assets/images/hexagonBackground.png'
 
 const images = [
   crown,
   rocket,
   fire,
+  star,
 ];
-const titles = ['King slayer', 'sky']
+const titles = ['crown', 'rocket', 'fire', 'star']
 
+const badges = ['crown', 'rocket', 'fire', 'star']
 
 const HexagonsGenerator = () => {
-
-  const badges = ['crown', 'rocket', 'fire']
-
   const isIcon = true;
+
   return (
 
     <div className='hexagonsContainer'>
       {
         badges.map((badge, index) => {
-          console.log('badge', badge, index);
+          const x = index % 2 === 0 && index > 0 ? index - 1 : -index;
+          const y = index % 2 === 0 && index > 0 ? index - 1 : index;
           return (
             <>
-              <Hexagon x={index % 2 === 0 ? index : -index} y={index} imageSrc={background} title={titles[index]} />
-              <Hexagon x={index % 2 === 0 ? index : -index} y={index} imageSrc={images[index]} isIcon={isIcon} />
+              <Hexagon x={x} y={y} imageSrc={background} title={titles[index]} />
+              <Hexagon x={x} y={y} imageSrc={images[index]} isIcon={isIcon} />
             </>
           )
         })
