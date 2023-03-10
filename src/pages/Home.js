109753f15ca7b819c415d64eb69131cffaa4bbf2
Nita1001 <devinedevops@gmail.com>
2023-03-10 15:26:10 +0200

    homePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import calendarImg from '../assets/images/Capture222.PNG'
 const Home = () => {
     return (
         <>
@@ -8,17 +9,15 @@ const Home = () => {
             </Helmet>
             <div className='home'>
                 <div className='grid-2'>
-                    <div className='homeCard'>
-                        Some field
+                    <div className='homeCard homeCard0'>
+                        <h1>The secret of making progress</h1>
+                        <h1> Is to get started</h1>
                     </div>
-                    <div className='homeCard'>
-                        Some field
+                    <div className='homeCard homeCard1'>
+                        <img src={calendarImg} alt='cal'></img>
                     </div>
-                    <div className='homeCard'>
-                        Some field
-                    </div>
-                    <div className='homeCard'>
-                        Some field
+                    <div className='homeCard homeCard2'>
+                        <button>Sign Up for free</button>
                     </div>
                 </div>
             </div>
