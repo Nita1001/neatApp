88bea3892d7a82c9ce790d9010122f6fc15b88fa
Nita1001 <devinedevops@gmail.com>
2023-03-12 00:17:36 +0200

    about

--- src/utils/backgrounds.js
+++ src/utils/backgrounds.js
@@ -3,6 +3,7 @@ const backgrounds = {
     '/schedule': 'scheduleBackground',
     '/profile': 'profileBackground',
     '/admin': 'adminBackground',
+    '/about': 'adminsBackground'
 };
 
 export default backgrounds;
