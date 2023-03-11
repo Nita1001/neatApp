e076de9617f971f4f9fe9f2fb41c24fe7df7af75
Nita1001 <devinedevops@gmail.com>
2023-03-11 15:28:40 +0200

    useHexIcon

--- src/components/HexagonsGenerator.js
+++ src/components/HexagonsGenerator.js
@@ -1,38 +1,16 @@
 import React from 'react'
+import useHexIcons from '../hooks/useHexIcons'
 import Hexagon from "./Hexagon";
-import crown from '../assets/icons/crown-colored.png'
-import rocket from '../assets/icons/rocket-front-gradient.png'
-import fire from '../assets/icons/fire-front-gradient.png'
-import star from '../assets/icons/star-front-gradient.png'
-import target from '../assets/icons/target-front-gradient.png'
-import calendar from '../assets/icons/calender-front-gradient.png'
-
 import background from '../assets/images/hexagonBackground.png'
-
-
-const generateIcon = (badge) => {
-  switch (badge) {
-    case 'crown':
-      return crown;
-    case 'rocket':
-      return rocket;
-    case 'fire':
-      return fire;
-    case 'star':
-      return star;
-    case 'target':
-      return target;
-    case 'calendar':
-      return calendar;
-    default:
-      return;
-  }
-}
 
 const badges = ['crown', 'rocket', 'fire', 'star', 'target', 'calendar']
 
 const HexagonsGenerator = () => {
-  const isIcon = true;
+
+  const {
+    generateIcon,
+    isIcon,
+  } = useHexIcons();
 
   return (
     <>
@@ -43,14 +21,12 @@ const HexagonsGenerator = () => {
           const even = (index % 2 === 0);
           const odd = (index % 2 !== 0);
           const lastIndex = (index === badges.length - 1);
-          const x = even && index > 0 ? pattern[index - 1] : lastIndex && odd && index > 1 ? 2 : odd && index > 1 ? -2 : -index;
-          const y = even && index > 0 ? pattern[index] : odd && index > 1 ? + 2 : index;
+          const x = (even && index > 0) ? pattern[index - 1] : (lastIndex && odd) && (index > 1) ? 2 : (odd && index > 1) ? -2 : -index;
+          const y = (even && index > 0) ? pattern[index] : (odd && index > 1) ? + 2 : index;
           return (
-
             <div className='hexagonsContainer' key={badge}>
               <Hexagon x={x} y={y} imageSrc={background} title={badges[index]} />
               <Hexagon x={x} y={y} imageSrc={generatedIcon} isIcon={isIcon} />
-
             </div>
           )
         })
---
+++ src/hooks/useHexIcons.js
@@ -0,0 +1,37 @@
+import crown from '../assets/icons/crown-colored.png'
+import rocket from '../assets/icons/rocket-front-gradient.png'
+import fire from '../assets/icons/fire-front-gradient.png'
+import star from '../assets/icons/star-front-gradient.png'
+import target from '../assets/icons/target-front-gradient.png'
+import calendar from '../assets/icons/calender-front-gradient.png'
+
+
+const useHexIcons = () => {
+    const isIcon = true;
+
+    const generateIcon = (badge) => {
+        switch (badge) {
+            case 'crown':
+                return crown;
+            case 'rocket':
+                return rocket;
+            case 'fire':
+                return fire;
+            case 'star':
+                return star;
+            case 'target':
+                return target;
+            case 'calendar':
+                return calendar;
+            default:
+                return;
+        }
+    }
+
+    return {
+        generateIcon,
+        isIcon
+    }
+}
+
+export default useHexIcons
