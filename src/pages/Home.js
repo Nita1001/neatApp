57ed4ef918488e18b56b49aa8ed5afde22181a17
Nita1001 <devinedevops@gmail.com>
2023-03-11 14:57:41 +0200

    homePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import calendarImg from '../assets/images/Capture222.PNG'
+import { Link } from 'react-router-dom'
 const Home = () => {
+
     return (
         <>
             <Helmet>
@@ -10,14 +12,14 @@ const Home = () => {
             <div className='home'>
                 <div className='grid-2'>
                     <div className='homeCard homeCard0'>
-                        <h1>The secret of making progress</h1>
-                        <h1> Is to get started</h1>
+                        <div className='headers'>
+                            <h1>The secret of making progress</h1>
+                            <h1>Is getting started</h1>
+                            <button ><Link to='/signUp'>Sign Up for free</Link></button>
+                        </div>
                     </div>
                     <div className='homeCard homeCard1'>
                         <img src={calendarImg} alt='cal'></img>
-                    </div>
-                    <div className='homeCard homeCard2'>
-                        <button>Sign Up for free</button>
                     </div>
                 </div>
             </div>
