f868d21bdea9f160941282267a55b5b2111460c1
Nita1001 <devinedevops@gmail.com>
2023-03-12 19:34:40 +0200

    refactor

--- src/hooks/useGiveBadges.js
+++ src/hooks/useGiveBadges.js
@@ -1,55 +1,50 @@
 import { useContext } from 'react'
-import { updateUsersData } from '../api/userServices'
+import { getUserById, updateUsersData } from '../api/userServices'
 import { LogInContext } from '../contexts/LogInContext'
-import { usersActions } from '../actions/usersActions'
 import useUsers from './useUsers'
+import { usersActions } from '../actions/usersActions'
 
 const useGiveBadges = () => {
     const { usersId } = useContext(LogInContext)
-    const { users, dispatch } = useUsers();
+    const { dispatch } = useUsers();
 
     const setCompletedTasksBadge = async () => {
+        const user = await getUserById(usersId);
+        const checkTasks = user.schedules;
+        const completedTasks = checkTasks.filter((task) => task.status === 'completed');
+        const giveBadge = completedTasks.length;
 
-        const user = users.find((user) => user.id === usersId);
-        if (user && user.schedules.length > 0) {
-            const checkTasks = user.schedules;
-            const completedTasks = checkTasks.filter((task) => task.status === 'completed');
-            const giveBadge = completedTasks.length;
-            if (giveBadge) {
-                let newBadge;
-                let badgeExists;
-                switch (giveBadge) {
-                    case 1:
-                        newBadge = { icon: 'star', title: 'first meeting' };
-                        badgeExists = user.badges && user.badges.some(
-                            (badge) => badge.icon === newBadge.icon && badge.title === newBadge.title);
-                        if (!badgeExists) {
-                            const updatedBadges = user.badges ? [...user.badges, newBadge] : [newBadge];
-                            const updatedUser = { ...user, badges: updatedBadges };
-                            await updateUsersData(usersId, updatedUser);
-                            dispatch({ type: usersActions.SET_USER_BADGES, payload: { id: usersId, badges: updatedBadges } });
-                        }
-                        break;
-                    case 5:
-                        newBadge = { icon: 'target', title: '5 meetings' };
-                        badgeExists = user.badges && user.badges.some(
-                            (badge) => badge.icon === newBadge.icon && badge.title === newBadge.title);
-                        if (!badgeExists) {
-                            const updatedBadges = user.badges ? [...user.badges, newBadge] : [newBadge];
-                            const updatedUser = { ...user, badges: updatedBadges };
-                            await updateUsersData(usersId, updatedUser);
-                            dispatch({ type: usersActions.SET_USER_BADGES, payload: { id: usersId, badges: updatedBadges } });
-                        }
-                        break;
-                    default:
-                        break;
-                }
+        const addBadge = async (newBadge) => {
+            const badgeExists = user.badges && user.badges.some(
+                (badge) => badge.icon === newBadge.icon && badge.title === newBadge.title);
+
+            if (!badgeExists) {
+                const updatedBadges = user.badges ? [...user.badges, newBadge] : [newBadge];
+                const updatedUser = { ...user, badges: updatedBadges };
+                await updateUsersData(usersId, updatedUser);
+                dispatch({
+                    type: usersActions.SET_USER_BADGES,
+                    payload: { id: usersId, badges: updatedBadges }
+                });
+                console.log('Received a new badge, check your profile');
             }
+        };
+
+        switch (giveBadge) {
+            case 1:
+                addBadge({ icon: 'star', title: 'first meeting' });
+                break;
+            case 5:
+                addBadge({ icon: 'target', title: '5 meetings' });
+                break;
+            default:
+                break;
         }
-    }
+    };
+
     return {
         setCompletedTasksBadge
     }
-};
+}
 
 export default useGiveBadges
