2ecc9a66969fd2c68a824170c0adff8fa4f88df4
Nita1001 <devinedevops@gmail.com>
2023-03-18 16:23:35 +0200

    refactoring

--- src/components/MeetingHoursList.js
+++ src/components/MeetingHoursList.js
@@ -1,5 +1,6 @@
 import useMeetingHoursList from '../hooks/useMeetingHoursList';
 import useMySchedule from '../hooks/useMySchedule';
+
 const MeetingHoursList = () => {
     const { handleMySchedule } = useMySchedule();
 
@@ -11,7 +12,8 @@ const MeetingHoursList = () => {
         availableHours,
         selectedTime,
         alreadyBooked,
-        setAlreadyBooked
+        setAlreadyBooked,
+        noAvailableTimeSlots
     } = useMeetingHoursList();
 
     const handleNewMeeting = (hour) => {
@@ -26,48 +28,51 @@ const MeetingHoursList = () => {
             {selectedDate && (
                 <div className='availableHoursContainer'>
                     {displayTimes && (
-                        availableHours.length > 0 ? (
-                            <>
-                                <h3>Available</h3>
-                                <ul className='hoursList text-center'>
-                                    {availableHours.map((hour) => (
-                                        <li className={
-                                            selectedTime ?
-                                                selectedTime.time === hour ?
-                                                    'activeHour' : '' : null
+                        <>
+                            {
+                                !noAvailableTimeSlots ?
+                                    <h3>Available</h3>
+                                    :
+                                    <p className='noneAvailableParagraph'>
+                                        None available meetings left on this day.
+                                    </p>
+                            }
+                            <ul className='hoursList text-center'>
+                                {availableHours.map((hour) => (
+                                    <li className={
+                                        selectedTime ?
+                                            selectedTime.time === hour ?
+                                                'activeHour' : '' : null
+                                    }
+                                        key={hour}
+                                        onClick={() => handleNewMeeting(hour)}
+                                    >{hour}
+                                    </li>
+                                ))}
+                            </ul>
+                            {
+                                selectedTime ?
+                                    <div className='setUpParagraph'>
+                                        {!alreadyBooked ?
+                                            <>
+                                                <p> Set Up Your Meeting? </p>
+                                                <button className='mineBtn'
+                                                    onClick={handleSetUpMeeting}>Mine
+                                                </button>
+                                            </>
+                                            :
+                                            <>
+                                                <p>Already booked a meeting</p>
+                                                <p>This Day</p>
+                                                <button className='editMeetingBtn'
+                                                    onClick={handleEditSchedule}> Edit meetings
+                                                </button>
+                                            </>
                                         }
-                                            key={hour}
-                                            onClick={() => handleNewMeeting(hour)}
-                                        >{hour}
-                                        </li>
-                                    ))}
-                                </ul>
-                                {
-                                    selectedTime ?
-                                        <div className='setUpParagraph'>
-                                            {!alreadyBooked ?
-                                                <>
-                                                    <p> Set Up Your Meeting? </p>
-                                                    <button className='mineBtn'
-                                                        onClick={handleSetUpMeeting}>Mine
-                                                    </button>
-                                                </>
-                                                :
-                                                <>
-                                                    <p>Already booked a meeting</p>
-                                                    <p>This Day</p>
-                                                    <button className='editMeetingBtn'
-                                                        onClick={handleEditSchedule}> Edit meetings
-                                                    </button>
-                                                </>
-                                            }
-                                        </div>
-                                        : null
-                                }
-                            </>
-                        ) : (
-                            <p className='noneAvailableParagraph'>None available meetings left on this day.</p>
-                        )
+                                    </div>
+                                    : null
+                            }
+                        </>
                     )}
                 </div>
             )}
--- src/hooks/useMeetingHoursList.js
+++ src/hooks/useMeetingHoursList.js
@@ -1,37 +1,32 @@
-import { useState, useEffect, useContext } from 'react'
-import uniqid from 'uniqid'
+import { useState, useEffect, useContext } from 'react';
+import uniqid from 'uniqid';
 import { getUsers } from '../api/userServices';
 import { SelectedDateContext } from '../contexts/SelectedDateContext';
 import { TIME_SLOTS } from '../utils/constants';
 import { LogInContext } from '../contexts/LogInContext';
 import { getUserById, updateUsersData } from '../api/userServices';
 
-
 const useMeetingHoursList = () => {
-    const { selectedDate, displayTimes, selectedTime, setSelectedTime } = useContext(SelectedDateContext);
-    const [availableHours, setAvailableHours] = useState([]);
+    const { selectedDate, displayTimes, selectedTime, setSelectedTime } = useContext(
+        SelectedDateContext
+    );
     const { usersId } = useContext(LogInContext);
+    const [availableHours, setAvailableHours] = useState(TIME_SLOTS);
     const [alreadyBooked, setAlreadyBooked] = useState(false);
+    const [noAvailableTimeSlots, setNoAvailableTimeSlots] = useState(false);
 
     useEffect(() => {
         const fetchUsers = async () => {
             try {
                 const users = await getUsers();
-                const availableTimeSlots = TIME_SLOTS.slice();
-                users.forEach((user) => {
-                    const schedules = user.schedules;
-                    if (schedules) {
-                        schedules.forEach((schedule) => {
-                            if (schedule.date === selectedDate) {
-                                const index = availableTimeSlots.indexOf(schedule.time);
-                                if (index > -1) {
-                                    availableTimeSlots.splice(index, 1);
-                                }
-                            }
-                        });
-                    }
-                });
-                setAvailableHours(availableTimeSlots);
+                const bookedHours = users.reduce((acc, user) => {
+                    const schedules = user.schedules || [];
+                    schedules
+                        .filter((schedule) => schedule.date === selectedDate)
+                        .forEach((schedule) => acc.add(schedule.time));
+                    return acc;
+                }, new Set());
+                setAvailableHours(TIME_SLOTS.filter((time) => !bookedHours.has(time)));
             } catch (error) {
                 console.error(error);
             }
@@ -39,20 +34,25 @@ const useMeetingHoursList = () => {
         fetchUsers();
     }, [selectedDate, selectedTime]);
 
+    useEffect(() => {
+        const available = availableHours.length === 0;
+        setNoAvailableTimeSlots(available);
+    }, [availableHours]);
+
     const handleSelectedTime = (hour) => {
         setSelectedTime({
             date: selectedDate,
             time: hour,
             id: uniqid(),
-            status: 'incomplete'
+            status: 'incomplete',
         });
-    }
+    };
 
     const handleSetUpMeeting = async () => {
         try {
             const user = await getUserById(usersId);
             if (user) {
-                const found = user.schedules.find((scheduled) => scheduled.date === selectedDate)
+                const found = user.schedules.find((scheduled) => scheduled.date === selectedDate);
                 if (found) {
                     return setAlreadyBooked(true);
                 } else {
@@ -66,6 +66,7 @@ const useMeetingHoursList = () => {
         setSelectedTime(null);
     };
 
+
     return {
         handleSelectedTime,
         availableHours,
@@ -75,8 +76,9 @@ const useMeetingHoursList = () => {
         setSelectedTime,
         handleSetUpMeeting,
         alreadyBooked,
-        setAlreadyBooked
+        setAlreadyBooked,
+        noAvailableTimeSlots,
     };
-}
+};
 
-export default useMeetingHoursList
+export default useMeetingHoursList;
